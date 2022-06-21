23e1829c4180822339be1a8cf2337aaf96dc8852
chaosmos <vitovich1987@gmail.com>
2022-06-21 20:40:04 +0300

    add standard mongoose validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,92 +1,107 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-                                         name:            {
-                                           type:     String,
-                                           required: [
-                                             true,
-                                             'A tour must have a name'
-                                           ],
-                                           unique:   true,
-                                           trim:     true
-                                         },
-                                         slug:            String,
-                                         rating:          {
-                                           type:    Number,
-                                           default: 4.5
-                                         },
-                                         duration:        {
-                                           type:     Number,
-                                           required: [
-                                             true,
-                                             'A tour must have a duration'
-                                           ]
-                                         },
-                                         maxGroupSize:    {
-                                           type:     Number,
-                                           required: [
-                                             true,
-                                             'A tour must have a group size'
-                                           ]
-                                         },
-                                         difficulty:      {
-                                           type:     String,
-                                           required: [
-                                             true,
-                                             'A tour must have a difficulty'
-                                           ]
-                                         },
-                                         ratingsAverage:  {
-                                           type:    Number,
-                                           default: 4.5
-                                         },
-                                         ratingsQuantity: {
-                                           type:    Number,
-                                           default: 0
-                                         },
-                                         price:           {
-                                           type:     Number,
-                                           required: [
-                                             true,
-                                             'A tour must have a price'
-                                           ]
-                                         },
-                                         priceDiscount:   Number,
-                                         summary:         {
-                                           type:     String,
-                                           trim:     true,
-                                           required: [
-                                             true,
-                                             'A tour must have a summary'
-                                           ]
-                                         },
-                                         description:     {
-                                           type: String,
-                                           trim: true
-                                         },
-                                         imageCover:      {
-                                           type:     String,
-                                           required: [
-                                             true,
-                                             'A tour must have a cover image'
-                                           ]
-                                         },
-                                         images:          [ String ],
-                                         createdAt:       {
-                                           type:    Date,
-                                           default: Date.now(),
-                                           select:  false
-                                         },
-                                         startDates:      [ Date ],
-                                         secretTour:      {
-                                           type:    Boolean,
-                                           default: false
-                                         }
-                                       }, {
-                                         toJSON:   { virtuals: true },
-                                         toObject: { virtuals: true }
-                                       });
+const tourSchema = new mongoose.Schema(
+{
+  name:            {
+    type:      String,
+    required:  [
+      true,
+      'A tour must have a name'
+    ],
+    unique:    true,
+    trim:      true,
+    maxLength: [
+      40,
+      'A tour name must + have less or equal then 40 characters'
+    ],
+    minLength: [
+      40,
+      'A tour name must have more or equal than 40 characters'
+    ]
+  },
+  slug:            String,
+  rating:          {
+    type:    Number,
+    default: 4.5
+  },
+  duration:        {
+    type:     Number,
+    required: [
+      true,
+      'A tour must have a duration'
+    ]
+  },
+  maxGroupSize:    {
+    type:     Number,
+    required: [
+      true,
+      'A tour must have a group size'
+    ]
+  },
+  difficulty:      {
+    type:     String,
+    required: [
+      true,
+      'A tour must have a difficulty'
+    ],
+    enum: {
+      values: ['easy', 'medium', 'difficulty'],
+      message: 'Difficult is either: easy, medium, difficult'
+    }
+  },
+  ratingsAverage:  {
+    type:    Number,
+    default: 4.5,
+    min: [1, "Rating must be above 1.0"],
+    max: [5, "Rating must be below 5.0"]
+  },
+  ratingsQuantity: {
+    type:    Number,
+    default: 0
+  },
+  price:           {
+    type:     Number,
+    required: [
+      true,
+      'A tour must have a price'
+    ]
+  },
+  priceDiscount:   Number,
+  summary:         {
+    type:     String,
+    trim:     true,
+    required: [
+      true,
+      'A tour must have a summary'
+    ]
+  },
+  description:     {
+    type: String,
+    trim: true
+  },
+  imageCover:      {
+    type:     String,
+    required: [
+      true,
+      'A tour must have a cover image'
+    ]
+  },
+  images:          [ String ],
+  createdAt:       {
+    type:    Date,
+    default: Date.now(),
+    select:  false
+  },
+  startDates:      [ Date ],
+  secretTour:      {
+    type:    Boolean,
+    default: false
+  }
+}, {
+ toJSON:   { virtuals: true },
+ toObject: { virtuals: true }
+});
 
 tourSchema.virtual('durationWeeks')
           .get(function () {
@@ -123,8 +138,9 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // Aggregation middleware
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline()
+      .unshift({ $match: { secretTour: { $ne: true } } })
 
   console.log(this);
   next();
