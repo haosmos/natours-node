b93dac2790d1a862b527ad5caa9f7da6e92554a5
chaosmos <vitovich1987@gmail.com>
2022-06-22 18:02:02 +0300

    implemented global error handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -12,20 +14,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-if (process.env.NODE_ENV === 'development') {
-
-}
 
 // 2) ROUTE HANDLERS
 
@@ -39,5 +33,11 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Page ${req.originalUrl} not found`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
