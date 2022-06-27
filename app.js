0d964cbc26604341645617a6348716f05242ccf6
chaosmos <vitovich1987@gmail.com>
2022-06-27 15:08:15 +0300

    added basic HTTP headers security via helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const rateLimit = require('express-rate-limit');
 const express = require('express');
 const logger = require('./utils/logger')
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 // const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
@@ -11,11 +12,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   //app.use(morgan('dev'));
   app.use(logger);
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -25,14 +31,20 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({
+  limit: '10kb'
+}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 // app.use((req, res, next) => {
 //   req.log.info("let's have a look on request and response, dude!")
 //   // req.requestTime = new Date().toISOString();
 //   next();
-// });
+// });D
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
