ad8f5e0aa4a3544cdf5e03c481b9d3018b1053a8
chaosmos <vitovich1987@gmail.com>
2022-07-04 21:29:01 +0300

    implemented tour page (frontend)

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -16,9 +16,21 @@ exports.getOverview = catchAsyncError(async (req, res) => {
      });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsyncError(async (req, res) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug })
+                         .populate({
+                           path: 'reviews',
+                           fields: 'review rating user'
+                         })
+  
+  // 2) Build template
+  
+  // 3) Render template
+  
   res.status(200)
      .render('tour', {
-       title: 'The Forest Hiker'
+       title: `${tour.name} Tour`,
+       tour
      });
-};
+});
