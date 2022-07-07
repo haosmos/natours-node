cfd39d95abb65b8c613b2537d80d91059f7ecde7
chaosmos <vitovich1987@gmail.com>
2022-07-07 20:42:15 +0300

    implemented updating user password with our API

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,14 @@
 // import '@babel/polyfill';
-import { login, logout } from './login';
-import { displayMap }    from './mapbox';
-import { updateDate }    from './updateSettings';
+import { login, logout }  from './login';
+import { displayMap }     from './mapbox';
+import { updateSettings } from './updateSettings';
 
 // DOM Elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // delegation
 if (mapBox) {
@@ -37,6 +38,27 @@ if (userDataForm) {
     const email = document.getElementById('email').value;
     const name = document.getElementById('name').value;
     
-    updateDate(name, email);
+    updateSettings({ name, email }, 'data');
+  })
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    
+    await updateSettings(
+        { passwordCurrent, password, passwordConfirm },
+        'password'
+    );
+    
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   })
 }
