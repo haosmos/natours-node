b6a7a2438be068c9c4d8555ec61d44151a6544cd
chaosmos <vitovich1987@gmail.com>
2022-07-15 21:44:42 +0300

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) MIDDLEWARES
+app.use(cors());
+// Access-Control-Allow-Origin *
+// app.use(cors({
+//   origin: ''
+// }))
+
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
