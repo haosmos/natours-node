c2bf1857557c309d0563fd9417f8730e8c6e66f3
chaosmos <vitovich1987@gmail.com>
2022-07-16 16:08:07 +0300

    fix stripe webhooks: changed req.rawBody to req.body

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -86,7 +86,7 @@ exports.webhookCheckout = catchAsyncError(async (req, res, next) => {
   let event;
   try {
     event = stripe.webhooks.constructEvent(
-        req.rawBody,
+        req.body,
         signature,
         process.env.STRIPE_WEBHOOK_SECRET
     );
